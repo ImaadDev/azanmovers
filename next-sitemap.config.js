d7d01ae8e1f686bdb78d8f506442eef0c5bbe35d
Codewithimaad <imadhussain6655@gmail.com>
2025-09-22 17:29:27 +0300

    Updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL,
+    siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
     generateRobotsTxt: true, // creates robots.txt automatically
     changefreq: 'daily',
     priority: 0.8,
